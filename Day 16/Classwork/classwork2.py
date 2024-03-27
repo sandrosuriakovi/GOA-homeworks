3413561cc48df9769d9f5b76f4f190634d413356
sandro suriakowi <sandrosuriakovi@gmail.com>
2024-03-27 19:25:56 +0400

    commit

---
+++ Day 16/Classwork/classwork2.py
@@ -0,0 +1,5 @@
+ID = ["Sandro","suriakowi"]
+print(ID[1],ID[0])
+
+
+
---
+++ Day 16/classwork3.py
@@ -0,0 +1,7 @@
+name = input("please enter your name:")
+surname = input("please enter your surname:")
+
+
+ID = [name,surname]
+print(ID[0],ID[1])
+
